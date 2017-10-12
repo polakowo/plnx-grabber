d5aacad25a2f48312613f01df54d96cd77b63b3b
Oleg Polakow <olegpolakow@gmail.com>
2017-10-12 21:02:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='plnx-grabber',
-    version='1.1', # MAJOR.MINOR
+    version='1.2', # MAJOR.MINOR
     description='Grabber of trade history from Poloniex exchange',
     url='https://github.com/polakowo/plnx-grabber',
     author='polakowo',
